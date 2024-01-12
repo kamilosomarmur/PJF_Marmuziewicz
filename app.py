601ea08304477c81a8b82094e01081ba6314440c
Kamil Marmuziewicz <kamilosomarmur@gmail.com>
2024-01-12 22:22:07 +0100

    Added DB, some buttons and table for our kanban board

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
+app.app_context().push()
+class Kanban(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    content=db.Column(db.String(200), nullable=False)
+    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>'%self.id
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():  # put application's code here
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Kanban(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Wystąpił błąd przy dodawaniu zadania'
+    else:
+        tasks = Kanban.query.order_by(Kanban.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == '__main__':
